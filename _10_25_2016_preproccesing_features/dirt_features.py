8f3583f2701de09c50e4fffac869ecbda9ef8436
Kate Boxer <ksbox12@gmail.com>
2016-11-16 14:01:57 -0500

    some feature exploration and feature generation extension

---
+++ _10_25_2016_preproccesing_features/dirt_features.py
@@ -0,0 +1,23 @@
+import cv2
+import logging
+import numpy
+
+logging.basicConfig(filename='default.log',level=logging.DEBUG)
+
+class FourRegionDirtDetection():
+	def run(self, path):
+		image = cv2.imread(path)
+		mask = cv2.inRange(image, np.array([61, 43, 0]) , np.array([153, 121, 93]))
+		res = cv2.bitwise_and(image,image, mask= mask)
+		width, height, t = res.shape
+		incre_w = int(width / 2)
+		incre_h = int(height / 2)
+		range_w = [(0,incre_w), (incre_w, 2* incre_w)]
+		range_h = [(0,incre_h), (incre_h, 2* incre_h)]
+		grid_images = []
+		for (w_0, w_1) in range_w:
+			for (h_0, h_1) in range_h:
+				im = res[w_0:w_1, h_0:h_1]
+				im_b= cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+				grid_images.append([[(w_0, w_1), (h_0, h_1)], cv2.countNonZero(im_b)/im.size])
+		return grid_images
--- _10_25_2016_preproccesing_features/feature_map.py
+++ _10_25_2016_preproccesing_features/feature_map.py
@@ -1,8 +1,10 @@
 import color_features
+import dirt_features
 
 feature_map = {
 	'hsv_color_histogram': color_features.HsvColorHistogram(),
-	'gray_color_histogram': color_features.GreyScaleHistogram()
+	'gray_color_histogram': color_features.GreyScaleHistogram(),
+	'dirt_feature_processing': dirt_features.FourRegionDirtDetection()
 	}
 
 def getFeature(path, feature_name):
